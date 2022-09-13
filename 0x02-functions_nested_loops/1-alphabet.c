8191b3d28bd0cff238bb7f29b0899baaf847599e
Wandile Ralph Dlamini <ralphdlamini3@gmail.com>
2022-09-13 11:24:23 -0700

    alphabets corrections

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - function that prints alphabets.
- *
- * Return: int
+ * print_alphabet - function that prints the alphabet, lowercase
+ * You can only use _putchar twice in your code
+ * Return: nothing.
  */
 
 void print_alphabet(void)
