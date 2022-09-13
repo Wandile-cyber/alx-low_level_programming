04d2f4f1af5173d79345926680058741b202e37c
Wandile Ralph Dlamini <ralphdlamini3@gmail.com>
2022-09-13 12:24:26 -0700

    fib sum

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,24 +1,31 @@
 #include "main.h"
+
+#include "stdio.h"
+
 /**
- * print_alphabet_x10 - function that prints 10 times the alphabet,
- * in lowercase
- * Return: nothing.
+ * main - program that finds and prints the sum of the even-valued terms, followed by a new line
+ *
+ * Return: int.
  */
 
-void print_alphabet_x10(void)
+int main(void)
 {
-	int l;
+	long i = 0;
+	long j = 1;
 	int n = 0;
+	int sum = 0;
+	long fib;
 
-	while (n < 10)
+	while (n < 4000000)
 	{
-		l = 'a';
-		while (l <= 'z')
-		{
-			_putchar(l);
-			l += 1;
-		}
-		_putchar(10);
+		fib = j + i;
+		if (fib % 2 == 0 )
+			sum += fib;
+
+		i = j;
+		j = fib;
 		n += 1;
 	}
+	printf("%ld\n", sum);
+	return (0);
 }
